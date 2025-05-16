4ecd2b530f26680d5a232d7b0fc19b55135d1bd4
shamyel richard <shamil.salmanov.04@inbox.ru>
2025-05-16 14:52:20 +0500

    Добавьте файлы проекта.

---
+++ LW_10_1/LW_10_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+void Function(double x) {
+    const int a = 5;
+    if (abs(x) < a) {
+        std::cout << "модуль значения х должен быть больше чем" << a << "\n";
+    }
+    else if (abs(x) == a) {
+        std::cout << "деление на ноль" << "\n";
+    }
+    std::cout << std::log((x + a) / (x - a));
+}
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    double x;
+    std::cin >> x;
+    Function(x);
+}
+
