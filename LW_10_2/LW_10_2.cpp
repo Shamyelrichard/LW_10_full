7a67515afb4c4087667cf17079c74d426f22a2d2
shamyel richard <shamil.salmanov.04@inbox.ru>
2025-05-16 15:28:55 +0500

    adding second task

---
+++ LW_10_2/LW_10_2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath> // добавил для возведения в степень
+
+double mean_geor(std::vector<double> arr) {
+    double product = 1; // серъёзно произведение это "product" какая досада
+    for (double elem : arr) {
+        product *= elem;
+    }
+    if (arr.size() % 2 == 1 && product < 0) {
+        std::cout << "ваша комбинация чисел не имеет среднего геометрического среди действительных чисел";
+        return 0;
+    }
+    std::cout << pow(product, arr.size());
+}
+
+int main(){
+    int counter;
+    char answer;
+    setlocale(LC_ALL, "Russian");
+    std::cout << "ВВедите число элемектов\n";
+    std::cin >> counter;
+    std::cout << "ВВедите элементы массива";
+    double elem;
+    bool flag = false;
+    std::vector < double > arr(counter);
+    for (int i = 0; i < counter; i++) {
+        std::cin >> elem;
+        if (elem == 0) {
+            std::cout << "Вы ввели '0' вы осазнаёте это? \n Д/Н";
+            std::cin >> answer; // пользователь думает, что от его ответа что-то зависит :)
+            flag = true;
+        }
+        arr.push_back(elem);
+    }
+    if (flag) { std::cout << "0 \n а вы чего ожидали?"; }
+    else {
+        mean_geor(arr);
+    }
+}
