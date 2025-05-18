8aa53d5733c09d16efa146cd28634221d5e6effe
shamyel richard <shamil.salmanov.04@inbox.ru>
2025-05-18 16:15:56 +0500

    task zwei

--- LW_10_2/LW_10_2.cpp
+++ LW_10_2/LW_10_2.cpp
@@ -1,18 +1,34 @@
 ﻿#include <iostream>
 #include <vector>
 #include <cmath> // добавил для возведения в степень
+#include <algorithm> // find
 
 bool mean_geor(std::vector<double> arr) {
     double product = 1; // серъёзно произведение это "product" какая досада
     for (double elem : arr) {
         product *= elem;
     }
-    if (arr.size() % 2 == 1 && product < 0) {
+    try
+    {
+        if (arr.size() % 2 == 1 && product < 0) {
+            throw std::exception("ваша комбинация чисел не имеет среднего геометрического среди действительных чисел");
+        }
+        auto it = std::find(arr.begin(), arr.end(), 0);
+        if (it != arr.end()) {
+            throw std::exception("ваш массив содержит ноль следовательно ответ 0");
+        }
+        std::cout << pow(product, (1.0 / arr.size()));
+    }
+    catch (const std::exception& ex)
+    {
+        std::cout << ex.what();
+    }
+    /*if (arr.size() % 2 == 1 && product < 0) {
         std::cout << "ваша комбинация чисел не имеет среднего геометрического среди действительных чисел";
         return 0;
     }
     std::cout << pow(product, double(1.0 / arr.size()));
-    return 1;
+    return 1;*/
 }
 
 int main(){
@@ -27,16 +43,7 @@ int main(){
     std::vector <double> arr;
     for (int i = 0; i < counter; i++) {
         std::cin >> elem;
-        if (elem == 0) {
-            std::cout << "Вы ввели '0' вы осазнаёте это? \n Д/Н \n"; // 1238326
-            std::cin >> answer; // пользователь думает, что от его ответа что-то зависит :)
-            flag = true;
-        }
         arr.push_back(elem);
     }
-
-    if (flag) { std::cout << "0 \nа вы чего ожидали?\n"; }
-    else {
-        mean_geor(arr);
-    }
+    mean_geor(arr);
 }
